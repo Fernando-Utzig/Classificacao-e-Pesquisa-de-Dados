21f334d53e429d39bc8945b3de90ce5bd34f0c33
Thiago Bischoff <76192049+Zigdeck@users.noreply.github.com>
2021-09-20 14:57:38 -0300

    Update Laboratorio 2 - QuickSort.py

--- Lab 2/Laboratorio 2 - QuickSort.py
+++ Lab 2/Laboratorio 2 - QuickSort.py
@@ -1,3 +1,17 @@
-aaaaaaaaaaaaaaa
-bbbbbbbbbbb
-cccccc
+def lomuto(vet, start, end, swaps):   #Particionamento de Lomuto
+    pivo = vet[start]
+    i = start + 1
+    for j in range(start + 1 , end + 1):
+        if vet[j] <= pivo:
+            swaps[0] += 1
+            # troca vet[i] com vet[j]
+            aux = vet[i]
+            vet[i] = vet[j]
+            vet[j] = aux
+            i += 1
+    
+    # Troca vet[i + 1] com o particionador
+    aux = vet[i - 1]
+    vet[i - 1] = vet[start]
+    vet[start] = aux
+    return i - 1
