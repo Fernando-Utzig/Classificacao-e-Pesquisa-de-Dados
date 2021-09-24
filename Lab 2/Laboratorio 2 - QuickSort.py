e77ef8c536f192f5b8b6f5653e8c96e6d44a0f45
Fernando Utzig <69490882+Fernando-Utzig@users.noreply.github.com>
2021-09-24 20:05:25 -0300

    Update Laboratorio 2 - QuickSort.py

--- Lab 2/Laboratorio 2 - QuickSort.py
+++ Lab 2/Laboratorio 2 - QuickSort.py
@@ -3,16 +3,15 @@ import statistics
 import time
 
 
-# Escolhe um particionador randomizado  (funcionando)
+# Escolhe um particionador randomizado
 def part_random(array, start, end):
     n = random.randint(start, end)
     aux = array[start]
     array[start] = array[n]
     array[n] = aux
 
-
-# Escolhe a mediana entre o primeiro, ultimo e valor do meio para particionador (funcionando)
-def part_mediana(array, start, end):
+# Escolhe a mediana entre o primeiro, último e valor do meio para usar como particionador
+def mediana_de_tres(array, start, end):
     meio = end // 2
     vetor_teste = [array[start], array[meio], array[end]]
     if statistics.median(vetor_teste) == array[meio]:
@@ -25,12 +24,11 @@ def part_mediana(array, start, end):
         array[end] = aux
 
 
-# Particionamento de Hoare  (funcionando)
+# Particionamento de Hoare
 def hoare_partition(array, start, end, swaps):
     pivot = array[start]
     i = start - 1
     j = end + 1
-
     while True:
         swaps[0] += 1
         i += 1
@@ -41,11 +39,10 @@ def hoare_partition(array, start, end, swaps):
             j -= 1
         if i >= j:
             return j
-
         array[i], array[j] = array[j], array[i]
 
 
-# Particionamento de Lomuto  (funcionando)
+# Particionamento de Lomuto
 def lomuto_partition(array, start, end, swaps):  # def lomuto(vet, start, end, swaps):
     pivo = array[start]
     i = start + 1
@@ -57,82 +54,98 @@ def lomuto_partition(array, start, end, swaps):  # def lomuto(vet, start, end, s
             array[i] = array[j]
             array[j] = aux
             i += 1
-
     aux = array[i - 1]
     array[i - 1] = array[start]
     array[start] = aux
     return i - 1
 
 
-# particionador aleatorio e particionamento de Hoare
+# Particionador aleatório
+# Particionamento de Hoare
 def quick_sort_random_hoare(array, start, end, recursao, swaps):
     if start < end:
         recursao[0] += 1
+        # Particionador
         part_random(array, start, end)
+        # Particionamento
         pi = hoare_partition(array, start, end, swaps)
-
         quick_sort_random_hoare(array, start, pi, recursao, swaps)
         quick_sort_random_hoare(array, pi + 1, end, recursao, swaps)
 
 
-# particionador mediana de 3 e particionamento de Hoare
-def quick_sort_part3_hoare(array, start, end, recursao, swaps):
+# Particionador mediana de 3
+# Particionamento de Hoare
+def quick_sort_med3hoare(array, start, end, recursao, swaps):
     if start < end:
         recursao[0] += 1
-        part_mediana(array, start, end)
+        # Particionador
+        mediana_de_tres(array, start, end)
+        # Particionamento
         pi = hoare_partition(array, start, end, swaps)
+        quick_sort_med3hoare(array, start, pi, recursao, swaps)
+        quick_sort_med3hoare(array, pi + 1, end, recursao, swaps)
 
-        quick_sort_part3_hoare(array, start, pi, recursao, swaps)
-        quick_sort_part3_hoare(array, pi + 1, end, recursao, swaps)
 
-
-# particionador aleatorio e particionamento de Lomuto
+# Particionador aleatório
+# Particionamento de Lomuto
 def quick_sort_random_lomuto(array, start, end, recursao, swaps):
     if start < end:
         recursao[0] += 1
+        # Particionador
         part_random(array, start, end)
+        # Particionamento
         pi = lomuto_partition(array, start, end, swaps)
 
         quick_sort_random_lomuto(array, start, pi - 1, recursao, swaps)
         quick_sort_random_lomuto(array, pi + 1, end, recursao, swaps)
 
 
-# particionador mediana de 3 e particionamento de Lomuto
-def quick_sort_part3_lomuto(array, start, end, recursao, swaps):
+# Particionador Mediana de 3
+# Particionamento de Lomuto
+def quick_sort_med3lomuto(array, start, end, recursao, swaps):
     if start < end:
         recursao[0] += 1
-        part_mediana(array, start, end)
+        # Particionador
+        mediana_de_tres(array, start, end)
+        # Particionamento
         pi = lomuto_partition(array, start, end, swaps)
 
-        quick_sort_part3_lomuto(array, start, pi - 1, recursao, swaps)
-        quick_sort_part3_lomuto(array, pi + 1, end, recursao, swaps)
+        quick_sort_med3lomuto(array, start, pi - 1, recursao, swaps)
+        quick_sort_med3lomuto(array, pi + 1, end, recursao, swaps)
 
 
 # MAIN
-tam = [100, 1000, 10000, 100000, 1000000]  # tamanhos dos vetores
-recursao = [-1]
-swaps = [0]
+tam = [100, 1000, 10000, 100000, 1000000]  # Tamanhos dos vetores
+recursao = [-1] # Variavel que conta as recursoes
+swaps = [0] # Variavel que conta os swaps
 
-with open('entrada2.txt', 'r') as f:
+with open('entrada-quicksort.txt', 'r') as f:
     for k in range(5):
+        # Lê a primeira linha do vetor
         data = f.readline()
         array = [int(i) for i in data.split()]
 
+        # Deleta o primeiro número da linha (tamanho do vetor)
         del array[0]
-
+        # Inicializa as variáveis start e end (inicio e final do vetor)
         start = 0
         end = len(array) - 1
 
+        # Copia o vetor 3 vezes para usar nas 4 saídas
         array2 = array.copy()
         array3 = array.copy()
         array4 = array.copy()
 
         recursao[0] = -1
         swaps[0] = 0
+
         inicio = time.time()
-        quick_sort_part3_hoare(array, start, end, recursao, swaps)
+        # Chamada da função do Quicksort particionador Mediana de 3 com particionamento Hoare
+        quick_sort_med3hoare(array, start, end, recursao, swaps)
         fim = time.time()
+        # Variável que guarda o tempo que a função levou para executar
         tempo = fim - inicio
+        # Guarda no arquivo
         with open('stats-mediana-hoare.txt', 'a') as arq:
             arq.write(f'TAMANHO ENTRADA {tam[k]}\n')
             arq.write(f'SWAPS {swaps[0]}\n')
@@ -141,9 +154,12 @@ with open('entrada2.txt', 'r') as f:
 
         recursao[0] = -1
         swaps[0] = 0
+
         inicio = time.time()
-        quick_sort_part3_lomuto(array2, start, end, recursao, swaps)
+        # Chamada da função do Quicksort particionador Mediana de 3 com particionamento Lomuto
+        quick_sort_med3lomuto(array2, start, end, recursao, swaps)
         fim = time.time()
+
         tempo = fim - inicio
         with open('stats-mediana-lomuto.txt', 'a') as arq:
             arq.write(f'TAMANHO ENTRADA {tam[k]}\n')
@@ -153,9 +169,12 @@ with open('entrada2.txt', 'r') as f:
 
         recursao[0] = -1
         swaps[0] = 0
+
         inicio = time.time()
+        # Chamada da função do Quicksort particionador Aleatório com particionamento Hoare
         quick_sort_random_hoare(array3, start, end, recursao, swaps)
         fim = time.time()
+
         tempo = fim - inicio
         with open('stats-aleatorio-hoare.txt', 'a') as arq:
             arq.write(f'TAMANHO ENTRADA {tam[k]}\n')
@@ -165,13 +184,15 @@ with open('entrada2.txt', 'r') as f:
 
         recursao[0] = -1
         swaps[0] = 0
+
         inicio = time.time()
+        # Chamada da função do Quicksort particionador Aleatório com particionamento Lomuto
         quick_sort_random_lomuto(array4, start, end, recursao, swaps)
         fim = time.time()
+
         tempo = fim - inicio
         with open('stats-aleatorio-lomuto.txt', 'a') as arq:
             arq.write(f'TAMANHO ENTRADA {tam[k]}\n')
             arq.write(f'SWAPS {swaps[0]}\n')
             arq.write(f'RECURSOES {recursao[0]}\n')
             arq.write(f'TEMPO {tempo} EM SEGUNDOS\n')
-
