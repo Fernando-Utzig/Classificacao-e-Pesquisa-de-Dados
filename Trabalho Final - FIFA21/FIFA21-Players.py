bf7fb7c5589f4de15fb7a0bb468c773e831d0d55
Thiago Bischoff <76192049+Zigdeck@users.noreply.github.com>
2021-11-19 00:28:49 -0300

    Update FIFA21-Players.py

--- Trabalho Final - FIFA21/FIFA21-Players.py
+++ Trabalho Final - FIFA21/FIFA21-Players.py
@@ -42,17 +42,29 @@ class Trie(object):
 
 
 def insert_trie(contents):
-    for x in range(0, len(contents)):     
-        test = 0                          
-        string = ''                              
-        for i in contents[x]:                       
-            if i == ',':                      
-                test += 1 
+    for x in range(0, len(contents)):
+        test = 0
+        string = ''
+        for i in contents[x]:
+            if i == ',':
+                test += 1
             if (test == 1) and ((i != '\n') and (i != ',')):
                 string = string + i
         tr.insert(string)
 
-            
+
+def polinomial_hash_id_players(word, M):
+    p = 11                                          # Primeiro número primo > 10,
+    hash_primario = 0
+    hash_final = 0
+    for i in word:
+        if (i != '\n') and (i != ' '):
+            num = int(i)
+            hash_primario = (p * hash_primario + num)             # Calculo polinomial
+            hash_final = hash_primario % M
+    return hash_final
+
+
 def polinomial_hash_nomes_players(word, M):
     p = 31                                          # Primeiro número primo > 26
     hash = 0
@@ -63,100 +75,89 @@ def polinomial_hash_nomes_players(word, M):
     return hash
 
 
-# Na pesquisa por nome, adiciona informações à pesquisa
-def pesquisa_por_nome(nome, hash):
-    pos_hash = polinomial_hash_nomes_players(nome, len(hash))
-    posiveis_nomes = hash[pos_hash]
+def calculo_media(hash_rating, vet_players, M_rating, hash_players, M_players):
+    for i in range(0, len(vet_players)):
+        soma = 0
+        count = 0
+        pos_hash_calc_nome = polinomial_hash_nomes_players(vet_players[i][1], M_players)
+        pos_hash_id_atual = polinomial_hash_id_players(vet_players[i][0], M_rating)
+        possiveis_notas = hash_rating[pos_hash_id_atual]
+        possiveis_nomes = hash_players[pos_hash_calc_nome]
+        for j in range(0, len(possiveis_notas)):
+            if vet_players[i][0] == possiveis_notas[j][1]:
+                soma = soma + float(possiveis_notas[j][2])
+                count += 1
+        if count != 0:
+            media = soma / count
+        else:
+            media = 0
+        for k in range(0, len(possiveis_nomes)):
+            if vet_players[i][1] == possiveis_nomes[k][1]:
+                hash_players[pos_hash_calc_nome][k].append(media)
+                hash_players[pos_hash_calc_nome][k].append(count)
+    return hash_players
+
+
+def pesquisa_por_nome(nome, hash_players):
+    pos_hash = polinomial_hash_nomes_players(nome, len(hash_players))
+    posiveis_nomes = hash_players[pos_hash]
     for i in range(0, len(posiveis_nomes)):
-        cursor1 = 0
-        cursor2 = 0
-        cont_cursor = 0
-        while cursor1 != ',':                           # Pega o nome do jogador entre a primeira e segunda virgula
-            cursor1 = posiveis_nomes[i][cont_cursor]
-            cont_cursor += 1
-        cursor_inicio = cont_cursor - 1
-        while cursor2 != ',':
-            cursor2 = posiveis_nomes[i][cont_cursor]
-            cont_cursor += 1
-        cursor_final = cont_cursor - 1
-        nome_jogador = posiveis_nomes[i][cursor_inicio + 1:cursor_final]
-        if nome == nome_jogador:
+        if nome == posiveis_nomes[i][1]:
             return posiveis_nomes[i]
-        
+
+
+# Abertura do arquivo de jogadores -------------------------------------------------------------------------------------
 with open('players.csv') as f:
-    players = f.readlines()
-del players[0]
+    players2 = f.readlines()
+del players2[0]
 
-
-# Monta a tabela hash usando o nome do jogador
-M = int(len(players) / 5)                               # Tamanho da tabela hash
-hash_players = [[] for _ in range(0, M)]
+# Separa os itens a cada virgula
+players = players2.copy()
 for i in range(0, len(players)):
-    cursor_inicio = 0
-    cursor_final = 0
-    cursor1 = 0
-    cursor2 = 0
-    cont_cursor = 0
-    while cursor1 != ',':                               # Pega o nome do jogador entre a primeira e segunda virgula
-        cursor1 = players[i][cont_cursor]
-        cont_cursor += 1
-    cursor_inicio = cont_cursor - 1
-    while cursor2 != ',':
-        cursor2 = players[i][cont_cursor]
-        cont_cursor += 1
-    cursor_final = cont_cursor - 1
-    nome_jogador_players = players[i][cursor_inicio + 1:cursor_final]
-    pos_hash = polinomial_hash_nomes_players(nome_jogador_players, M)
-    hash_players[pos_hash].append(players[i])
-    
-    
-# Abertura do arquivo rating e criação do hash desse arquivo -----------------------------------------------------------
+    a = players[i].split(',')
+    players[i] = a
+
+# Cria uma tabela Hash para jogadores utilizando os nomes
+M_players = int(len(players) / 5)
+hash_players = [[] for _ in range(0, M_players)]
+for j in range(0, len(players)):
+    pos_hash_players = polinomial_hash_nomes_players(players[j][1], M_players)
+    players[j][len(players[j])-1] = players[j][len(players[j])-1][:-1]
+    hash_players[pos_hash_players].append(players[j])
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Abertura do arquivo de notas
 with open('minirating.csv') as f:
     rating = f.readlines()
-del rating[0]
+del(rating[0])
 
-M_rating = int(len(rating) / 4)
-hash_rating = [[] for _ in range(0, M_rating)]
 for i in range(0, len(rating)):
-    cursor_inicio = 0
-    cursor_final = 0
-    cursor1 = 0
-    cursor2 = 0
-    cont_cursor = 0
-    while cursor1 != ',':                               # Pega o ID do jogador entre a primeira e segunda virgula
-        cursor1 = rating[i][cont_cursor]
-        cont_cursor += 1
-    cursor_inicio = cont_cursor - 1
-    while cursor2 != ',':
-        cursor2 = rating[i][cont_cursor]
-        cont_cursor += 1
-    cursor_final = cont_cursor - 1
-    id_jogador_rating = rating[i][cursor_inicio + 1:cursor_final]
-    pos_hash = polinomial_hash_id_players(id_jogador_rating, M_rating)
-    hash_rating[pos_hash].append(rating[i])
+    a = rating[i].split(',')
+    rating[i] = a
 
+M_rating = int(len(rating) / 5)
+hash_rating = [[] for _ in range(0, M_rating)]
+for j in range(0, len(rating)):
+    pos_hash_rating = polinomial_hash_id_players(rating[j][1], M_rating)
+    hash_rating[pos_hash_rating].append(rating[j])
+# ----------------------------------------------------------------------------------------------------------------------
 
+hash_players_stg2 = calculo_media(hash_rating, players, M_rating, hash_players, M_players)
 
 tr = Trie()
-insert_trie(players)
+insert_trie(players2)
+
 
 name = input("Digite um nome para pesquisar:\n")
 name = name.capitalize()
 pesquisa = (tr.search(name))
+
+print('ID\t\tNOME\t\tPOS\t\tNOTA\t\tCOUNT')
 for x in pesquisa:
-    info = pesquisa_por_nome(x, hash_players)
-    print(info)
+    info = pesquisa_por_nome(x, hash_players_stg2)
+    for i in range(0, len(info)):
+        if i == len(info) - 1:
+            print(info[i], end="\n")
+        else:
+            print(info[i], end="\t")
 
-"""
-import csv
-
-with open(''minirating.csv', ‘r’) as arquivo_csv:  
-    leitor = csv.reader(arquivo_csv, delimiter=’,’)    
-    for coluna in leitor:        
-        print(coluna)
-
-
-# [‘Nome’, ‘Profissao’]
-# [‘Renato’, ‘Programador’]
-# [‘Ana’, ‘Faxineira’]
-"""
